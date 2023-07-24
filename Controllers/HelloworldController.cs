c9323afc61e68cbb7244049714b9462ba924828e
Med Amine <chakrounmedamine19@gmail.com>
2023-07-24 09:15:30 +0100

    first commit

---
+++ Controllers/HelloworldController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloworldController : Controller
+    {
+        public IActionResult Index()
+        {
+            dogviewmodel dog = new dogviewmodel()
+            { Name = "django", Age = 20 };
+            return View(dog);
+        }
+        public IActionResult Create()            
+        {
+            /*var dogo = new dogviewmodel();*/
+            return View(/*doggo*/);
+        }
+
+        public IActionResult CreateDog(dogviewmodel dogviewmodel)
+        {
+            return View("index");
+        }
+        public string hello()
+        {
+            return "Whos there";
+        }
+    }
+}
