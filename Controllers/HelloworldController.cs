e01ea539530a11a80234ca9fc67a4da3d17f597f
Med Amine <chakrounmedamine19@gmail.com>
2023-07-24 09:52:54 +0100

    data binding done

--- Controllers/HelloworldController.cs
+++ Controllers/HelloworldController.cs
@@ -1,25 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HelloworldController : Controller
     {
+
+        private static List<dogviewmodel> dogs = new List<dogviewmodel>();
         public IActionResult Index()
         {
-            dogviewmodel dog = new dogviewmodel()
+            /*dogviewmodel dog = new dogviewmodel()
             { Name = "django", Age = 20 };
-            return View(dog);
+            return View(dog);*/
+            return View(dogs);
         }
         public IActionResult Create()            
         {
-            /*var dogo = new dogviewmodel();*/
-            return View(/*doggo*/);
+            var dogo = new dogviewmodel();
+            return View(dogo);
         }
 
         public IActionResult CreateDog(dogviewmodel dogviewmodel)
         {
-            return View("index");
+            dogs.Add(dogviewmodel);
+            //return View("index");
+            return RedirectToAction(nameof(Index));
         }
         public string hello()
         {
